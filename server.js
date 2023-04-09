2c396c795688b1f1d60a54f2afa272252e405ff1
Ryebread5555 <Rypetty55@gmail.com>
2023-04-09 18:57:09 -0500

    fixed server issues

--- server.js
+++ server.js
@@ -8,13 +8,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-app
+app.use(require('./routes'));
 
 // setup mongo db 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
-  useFindAndModify: false,
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
+
 
 app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
