012e3746e12b957696390f11483a09ed5a45d617
Ryebread5555 <Rypetty55@gmail.com>
2023-04-09 18:57:20 -0500

    fixed spelling errors

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -107,22 +107,23 @@ const thoughtController = {
         .catch(err => res.status(400).json(err));
     },
     
-    // delete reactions
-    deleteReaction({ params }, res) {
-        Thought.findOneAndUpdate(
-            { _id: params.thoughtId },
-            { $pull: { reaction: { reactionId: params.reactionId }}},
-            { new: true }
-        )
-        .then(dbThoughtData => {
-            if (!dbThoughtData) {
-                res.status(404).json({ message: 'No thought has been found with that id'});
-                return;
-            }
-            res.json(dbThoughtData);
-        })
-        .catch(err => res.status(400).json(err));
-    }
+// delete reactions
+deleteReaction({ params }, res) {
+    Thought.findOneAndUpdate(
+        { _id: params.thoughtId },
+        { $pull: { reactions: { reactionId: params.reactionId }}},
+        { new: true }
+    )
+    .then(dbThoughtData => {
+        if (!dbThoughtData) {
+            res.status(404).json({ message: 'No thought has been found with that id'});
+            return;
+        }
+        res.json(dbThoughtData);
+    })
+    .catch(err => res.status(400).json(err));
+}
+
 };
 
 module.exports = thoughtController;
